d1ceec1feb5ff1b075a54c08bbc1cb570bf1d9d1
Shweta <shwetakoffficial@gmail.com>
2024-12-02 11:04:17 +0530

    setting server

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+//1.create express
+const express=require('express');
+const app=express();
+const dotenv=require('dotenv')
+
+dotenv.config();
+const cors=require('cors')
+
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('hello world')
+});
+module.exports=app;
+
